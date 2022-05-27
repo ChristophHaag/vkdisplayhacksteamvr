d3e8c1f033b7596fb43a38179ae95e392e42c5a2
Christoph Haag <christoph.haag@collabora.com>
2022-05-27 18:55:46 +0200

    add license header to source file

--- vkdisplayinfo.c
+++ vkdisplayinfo.c
@@ -1,3 +1,11 @@
+// Copyright 2019-2022, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Simple Vulkan display info
+ * @author Christoph Haag <christoph.haag@collabora.com>
+ */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
